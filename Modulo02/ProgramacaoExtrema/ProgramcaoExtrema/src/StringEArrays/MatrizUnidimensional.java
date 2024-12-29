1fb7ecc9515738da42b9b9cf9286449d5b296fd7
Eduardo Lima <duduzx02@gmail.com>
2024-12-28 22:38:57 -0300

    Strings e Arrays

---
+++ Modulo02/ProgramacaoExtrema/ProgramcaoExtrema/src/StringEArrays/MatrizUnidimensional.java
@@ -0,0 +1,17 @@
+package StringEArrays;
+
+public class MatrizUnidimensional {
+    public static void main(String[] args) {
+        int[] resultadoArray = new int[6];
+        resultadoArray[0] = 10;
+
+        System.out.println("Posições do Array: " + resultadoArray[0]);
+
+
+        // outra forma de inicializar
+
+        int[] numeros = {1, 2, 3, 4};
+
+
+    }
+}
